685e387d58c482ae30ebc864830643f90731ad53
soultan.ali.h <soultan.ali.h@gmail.com>
2022-11-25 10:51:30 +0800

    -

--- client/src/components/liveMonitoring/NotificationList.js
+++ client/src/components/liveMonitoring/NotificationList.js
@@ -167,12 +167,15 @@ const NotificationList = ({
         </div>
         <div className="load-more d-flex justify-content-center mt-3">
           <a
-            className="p-medium"
+            className={
+              "p-medium" +
+              (data.length < datalimit ? " disabled text-secondary" : "")
+            }
             onClick={() => {
               setDatalimit(datalimit + 10);
             }}
           >
-            Load More
+            {data.length < datalimit ? "No More Data" : "Load More"}
           </a>
         </div>
       </div>
